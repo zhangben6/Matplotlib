ea637d3e1e30746d18413639dd5ce741eca64bf4
zhangben <1457287508@qq.com>
2018-11-03 15:24:54 +0800

    模拟多次随机漫步

--- rw_visual.py
+++ rw_visual.py
@@ -2,7 +2,12 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values,rw.y_values,s=5)
-plt.show()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values,rw.y_values,s=5)
+    plt.show()
+
+    keep_running = input("Make another walk?(y/n)")
+    if keep_running == 'n':
+        break
