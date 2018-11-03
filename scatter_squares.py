1ff27af0f19e66aa5dff2bccf09a8ccdc5fe2b23
zhangben <1457287508@qq.com>
2018-11-03 14:27:08 +0800

    绘制一系列的点

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,9 @@
 '''使用scatter()绘制散点图并设置其样式'''
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4,s=200)
+x_values = [1,2,3,4,5]
+y_values = [1,4,9,16,25]
+plt.scatter(x_values,y_values,s=30)
 
 #设置图标标题并给坐标轴加上标签
 plt.title("Square Number",fontsize=24)
