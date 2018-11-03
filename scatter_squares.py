3301ad984a338fadf599364b954ababadf06071e
zhangben <1457287508@qq.com>
2018-11-03 14:24:16 +0800

    设置x,y坐标的标题和样式

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,5 +1,12 @@
 '''使用scatter()绘制散点图并设置其样式'''
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4)
+plt.scatter(2,4,s=200)
+
+#设置图标标题并给坐标轴加上标签
+plt.title("Square Number",fontsize=24)
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Squares of value",fontsize=14)
+#设置刻度标记的大小
+plt.tick_params(axis="both",which="major",labelsize=14)
 plt.show()
